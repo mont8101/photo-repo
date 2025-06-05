aa98958bd50c5b365f569112d156b1b47ae48deb
mont8101 <55461476+mont8101@users.noreply.github.com>
2025-06-05 15:44:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SpringMongoPhotoRepo
+A spring and mongo project for storing photos uploaded into a db
---
+++ src/test/java/com/personal/ben/photorepo/PhotoRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.ben.photorepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoRepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
